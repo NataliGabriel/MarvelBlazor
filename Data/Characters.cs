c265c00201c95de9859a05a79fb17b568e6618c9
NataliGabriel <bielnatali7@gmail.com>
2023-06-16 16:59:39 -0300

    commitando

---
+++ Data/Characters.cs
@@ -0,0 +1,93 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MarvelApi.Data
+{
+    public class ResponseData
+    {
+        public string Code { get; set; }
+        public string Status { get; set; }
+        public string Copyright { get; set; }
+        public string AttributionText { get; set; }
+        public string AttributionHTML { get; set; }
+        public string Etag { get; set; }
+        public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        public string Offset { get; set; }
+        public string Limit { get; set; }
+        public string Total { get; set; }
+        public string Count { get; set; }
+        public List<MarvelApiResponse> Results { get; set; }
+    }
+    public class MarvelApiResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Modified { get; set; }
+        public Thumbnail Thumbnail { get; set; }
+        public Comics Comics { get; set; }
+        public Series Series { get; set; }
+        public Stories Stories { get; set; }
+        public Events Events { get; set; }
+        public List<Url> Urls { get; set; }
+    }
+
+    public class Thumbnail
+    {
+        private string _path;
+        public string Path
+        {
+            get { return _path; }
+            set { _path = value + ".jpg"; }
+        }
+        public string Extension { get; set; }
+    }
+
+    public class Comics
+    {
+        public int Available { get; set; }
+        public string CollectionURI { get; set; }
+        public List<Item> Items { get; set; }
+        public int Returned { get; set; }
+    }
+
+    public class Series
+    {
+        public int Available { get; set; }
+        public string CollectionURI { get; set; }
+        public List<Item> Items { get; set; }
+        public int Returned { get; set; }
+    }
+
+    public class Stories
+    {
+        public int Available { get; set; }
+        public string CollectionURI { get; set; }
+        public List<Item> Items { get; set; }
+        public int Returned { get; set; }
+    }
+
+    public class Events
+    {
+        public int Available { get; set; }
+        public string CollectionURI { get; set; }
+        public List<Item> Items { get; set; }
+        public int Returned { get; set; }
+    }
+
+    public class Item
+    {
+        public string ResourceURI { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class Url
+    {
+        public string Type { get; set; }
+        public string url { get; set; }
+    }
+}
---
+++ Data/MarvelAPIService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+namespace MarvelApi.Data
+{
+    public class MarvelAPIService
+    {
+        private const string BaseUrl = "https://gateway.marvel.com/v1/public/";
+        private const string PublicKey = "fbb485d3da2fb3a3ae43e7e1dae49944";
+        private const string PrivateKey = "b47a0c2fd2b39ccc7c5e6df0eea97b662d7236e4";
+
+        private readonly HttpClient _httpClient;
+
+        public MarvelAPIService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(BaseUrl);
+        }
+        private string GerarHash(
+            string ts, string publicKey, string privateKey)
+        {
+            byte[] bytes =
+                Encoding.UTF8.GetBytes(ts + privateKey + publicKey);
+            var gerador = MD5.Create();
+            byte[] bytesHash = gerador.ComputeHash(bytes);
+            return BitConverter.ToString(bytesHash)
+                .ToLower().Replace("-", String.Empty);
+        }
+        public ResponseData GetCharacterData()
+        {
+            var timestamp = DateTime.Now.Ticks.ToString();
+            var hash = GerarHash(timestamp, PublicKey, PrivateKey);
+            var url = $"{BaseUrl}characters?ts={timestamp}&apikey={PublicKey}&hash={hash}";
+            var response = _httpClient.GetStringAsync(url).Result;
+
+            return JsonConvert.DeserializeObject<ResponseData>(response);
+        }
+    }
+
+}
